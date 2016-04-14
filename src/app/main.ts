72ed2ec93ba3ec80401bb57859ac84ad64d5a9a5
shogogg <shogo@studiofly.net>
2016-04-14 23:20:42 +0900

    Optimize imports

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,5 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {Application} from './components/application';
-
-import './styles/main.styl';
+import {bootstrap} from "angular2/platform/browser";
+import {Application} from "./components/application";
+import "./styles/main.styl";
 
 bootstrap(Application);
